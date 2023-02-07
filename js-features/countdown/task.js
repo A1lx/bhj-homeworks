d59b218063a9e20591bef28f2c3f07f84d825413
Aleksey Klimov <alx111wrk@gmail.com>
2023-02-07 19:45:59 +0300

    1.1 дз

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,37 +1,37 @@
-const timer = document.getElementById("timer");
-
-function startTimer() {
-  if (+timer.textContent === 0) {
-    window.alert("Вы победили в конкурсе!");
-    clearInterval(timerId);
-  } else {
-    timer.textContent = +timer.textContent - 1;
-  }
-}
-
-let timerId = setInterval(startTimer, 1000);
-
-
 // const timer = document.getElementById("timer");
-// const countDownDate = new Date().setSeconds(new Date().getSeconds() + Number(timer.textContent));
 
 // function startTimer() {
-//   const currentDate = new Date().getTime();
-//   const countDown = new Date(countDownDate).getTime();
-//   const diff = (countDown - currentDate) / 1000;
-
-//   if (diff < 0) {
+//   if (+timer.textContent === 0) {
 //     window.alert("Вы победили в конкурсе!");
 //     clearInterval(timerId);
-//     //window.location = "https://www.google.ru";
 //   } else {
-//     let hh = String(Math.floor((diff % (60 * 60 * 24)) / (60 * 60))).padStart(2, "0");
-//     let mm = String(Math.floor((diff % (60 * 60)) / 60)).padStart(2, "0");
-//     let ss = String(Math.floor(diff % 60)).padStart(2, "0");
-
-//     timer.textContent = hh + ":" + mm + ":" + ss;
-//   }  
+//     timer.textContent = +timer.textContent - 1;
+//   }
 // }
 
-// startTimer();
 // let timerId = setInterval(startTimer, 1000);
+
+
+const timer = document.getElementById("timer");
+const countDownDate = new Date().setSeconds(new Date().getSeconds() + Number(timer.textContent));
+
+function startTimer() {
+  const currentDate = new Date().getTime();
+  const countDown = new Date(countDownDate).getTime();
+  const diff = (countDown - currentDate) / 1000;
+
+  if (diff < 0) {
+    window.alert("Вы победили в конкурсе!");
+    clearInterval(timerId);
+    window.location = "https://www.google.ru";
+  } else {
+    let hh = String(Math.floor((diff % (60 * 60 * 24)) / (60 * 60))).padStart(2, "0");
+    let mm = String(Math.floor((diff % (60 * 60)) / 60)).padStart(2, "0");
+    let ss = String(Math.floor(diff % 60)).padStart(2, "0");
+
+    timer.textContent = hh + ":" + mm + ":" + ss;
+  }  
+}
+
+startTimer();
+let timerId = setInterval(startTimer, 1000);
