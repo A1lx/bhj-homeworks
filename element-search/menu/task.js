05364d01f7ece70e45d771d2ded026677d330c2e
Aleksey Klimov <alx111wrk@gmail.com>
2023-02-09 23:12:14 +0300

    Внесены правки, 2 вер

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -7,6 +7,10 @@ for (let i = 0; i < arrMenuLinks.length; i++) {
   const item = link.closest('.menu__item');
   const subMenu = item.querySelector('.menu_sub');
   link.onclick = function () {
+    if (subMenu.className.includes("menu_active")) {
+      subMenu.className = "menu menu_sub";
+      return false;
+    }
     if (subMenu) {
       subMenu.className = "menu menu_sub menu_active";
       return false;
